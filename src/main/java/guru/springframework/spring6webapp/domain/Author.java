9bfd3842a2df2e7d1498034e78b214f61e9364ec
Chris Yager <43963367+dcyager359@users.noreply.github.com>
2024-09-19 12:32:55 -0400

    add equals/hashcode

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -37,6 +38,29 @@ public class Author {
 
     public String getFirstName() {
         return firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 
     public void setFirstName(String firstName) {
--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -9,7 +10,6 @@ import java.util.Set;
  */
 @Entity
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -33,7 +33,7 @@ public class Book {
         return id;
     }
 
-    public void setId(Long id) {
+        public void setId(Long id) {
         this.id = id;
     }
 
@@ -52,4 +52,28 @@ public class Book {
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
 }
